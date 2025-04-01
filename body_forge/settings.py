dad815063b9c5ed5c0170f37aa419114fc9d9ad5
zlatin-r <zlatinrusev@gmail.com>
2025-04-01 20:18:32 +0200

    changes added

--- body_forge/settings.py
+++ body_forge/settings.py
@@ -131,3 +131,5 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.AppUser'
