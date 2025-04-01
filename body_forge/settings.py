98d845b4af96920a272c4ebbe1923ef541187be9
zlatin-r <zlatinrusev@gmail.com>
2025-04-01 17:40:08 +0200

    init .gitignore file

--- body_forge/settings.py
+++ body_forge/settings.py
@@ -70,13 +70,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'body_forge.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "body_forge_db",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
