1b01ff2c502ed452af02d0d7c667186b208314e2
zlatin-r <zlatinrusev@gmail.com>
2025-04-03 16:57:57 +0200

    changes added

--- body_forge/settings.py
+++ body_forge/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [ BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / "mediafiles"
