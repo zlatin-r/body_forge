beed43d7fb52b43d8be75eea2835a9de268e05d2
zlatin-r <zlatinrusev@gmail.com>
2025-05-01 15:21:56 +0200

    changes added

--- body_forge/common/management/commands/migratestart.py
+++ body_forge/common/management/commands/migratestart.py
@@ -2,10 +2,8 @@ from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
 
-
-
 class Command(BaseCommand):
-    help = "Runs the commands 'makemigrations' and 'migrate'"
+    help = "Runs the commands 'makemigrations', 'migrate' and 'runserver'"
 
     def handle(self, *args, **options):
         call_command("makemigrations")
